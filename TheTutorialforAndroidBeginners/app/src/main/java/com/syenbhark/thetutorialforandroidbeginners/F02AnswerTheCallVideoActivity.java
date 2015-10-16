43f715de6891dfa142f82b9dc61709d235c27db1
theoyoung <psyflos@gmail.com>
2015-10-16 23:04:45 +0900

    may be succeed in playing the videios with fingers image and synchronizing with user's ringtone.

--- TheTutorialforAndroidBeginners/app/src/main/java/com/syenbhark/thetutorialforandroidbeginners/F02AnswerTheCallVideoActivity.java
+++ TheTutorialforAndroidBeginners/app/src/main/java/com/syenbhark/thetutorialforandroidbeginners/F02AnswerTheCallVideoActivity.java
@@ -4,11 +4,11 @@ import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.WindowManager;
 import android.widget.VideoView;
 
-import java.util.concurrent.CyclicBarrier;
 
 /**
  * Shows the simulation video which is one of common ways to answer the incoming call.
@@ -16,8 +16,9 @@ import java.util.concurrent.CyclicBarrier;
  * @author Syen
  */
 public class F02AnswerTheCallVideoActivity extends AppCompatActivity {
-    final CyclicBarrier gate = new CyclicBarrier(3);
     Ringtone ringtone;
+    VideoView video;
+
     /**
      * Plays the simulation video with removing its action bar and title bar.
      *
@@ -34,11 +35,9 @@ public class F02AnswerTheCallVideoActivity extends AppCompatActivity {
         getSupportActionBar().hide();
 
 
-
-
         // Gets unlock video and plays it.
         // cite: http://pngimg.com/download/946 image
-        VideoView video = (VideoView) findViewById(R.id.videoView_incoming_call);
+        video = (VideoView) findViewById(R.id.videoView_incoming_call);
         video.setVideoURI(Uri.parse(getString(R.string.uri_prefix) + R.raw.incoming_call));
         video.requestFocus();
         video.start();
@@ -47,72 +46,43 @@ public class F02AnswerTheCallVideoActivity extends AppCompatActivity {
         // cite: http://stackoverflow.com/questions/2618182/how-to-play-ringtone-alarm-sound-in-android
         ringtone = RingtoneManager.getRingtone(getApplicationContext(), RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE));
 
-
-        Thread thread1 = new Thread() {
-            @Override
-            public void run() {
-                try {
-
-                    while(true) {
-
-                        sleep(3000);
-                        ringtone.play();
-
-                    }
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        };
-
-        Thread thread3 = new Thread() {
-            @Override
-            public void run() {
-                try {
-
-                    while(true) {
-
-                        sleep(9000);
-                        ringtone.stop();
-
-                    }
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        };
-
-
-
-        Thread thread2 = new Thread() {
-            @Override
-            public void run() {
-                try {
-
-                    while(true) {
-
-                        sleep(R00ConstantTimeValues.INCOMING_CALL_VIDEO_TIME_OUT);
-                        finish();
-                    }
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        };
-
-        thread1.start();
-        thread3.start();
-        thread2.start();
-        /*
-        // After watching it, finishes the activity.
+        // Starts playing the ringtone after starting the video few minutes.
         new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                ringtone.play();
+            }
+        }, R00ConstantTimeValues.RINGTONE_START_TIME);
+
+        // Stops playing the ringtone before stopping the video.
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                ringtone.stop();
+            }
+        }, R00ConstantTimeValues.RINGTONE_STOP_TIME);
+
+
+        // After watching it, finishes the activity.
+        new android.os.Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 // close this activity
                 finish();
             }
         }, R00ConstantTimeValues.INCOMING_CALL_VIDEO_TIME_OUT);
-        */
+    }
+
+    /**
+     *  Stops playing the ringtone and destroys the activity when you cannot see the video.
+     *  For example, when you press the home key or hold key or back key.
+     *  This is for trying to synchronize playing the video with user's ringtone.
+     */
+    @Override
+    public void onStop() {
+        super.onStop();
+        ringtone.stop();
+        finish();
     }
 
 }
--- TheTutorialforAndroidBeginners/app/src/main/java/com/syenbhark/thetutorialforandroidbeginners/R00ConstantTimeValues.java
+++ TheTutorialforAndroidBeginners/app/src/main/java/com/syenbhark/thetutorialforandroidbeginners/R00ConstantTimeValues.java
@@ -10,6 +10,8 @@ public class R00ConstantTimeValues {
     final static int VIBRATE_SHORT = 30;
     final static int VIBRATE_LONG = 500;
     final static int COMMON_VIDEO_TIME_OUT = 12000;
+    final static int RINGTONE_START_TIME = 3000;
+    final static int RINGTONE_STOP_TIME = 12000;
     final static int INCOMING_CALL_VIDEO_TIME_OUT = 16000;
     final static int TOAST_SHORT_TIME = 2000;
 }
